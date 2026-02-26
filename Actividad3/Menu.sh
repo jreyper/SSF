270205179972eb948019e174547c60ae6248a86c
Alberto <albertocarballohernandez@gmail.com>
2026-02-26 19:45:06 +0000

    hecho por script

---
+++ Actividad3/Menu.sh
@@ -0,0 +1,16 @@
+#!/urs/bin/bash
+
+set -euo pipefail
+echo "1. Espacio libre en disco (%)"
+echo "2. Espacio libre (tamaño)"
+echo "3. Usuario actual y nombre de la máquina"
+echo "4. Números de usuarios en la máquina"
+echo "5. Espacio usado en tu carpeta"
+read -p "Elija una opción: " opcion
+
+case $opcion in
+1)df -h
+2)df -h
+3)echo "Usuario:" $(whoami)
+4)wc -l /etc/passwd
+5)du -sh $HOME;;

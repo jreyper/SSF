c03214727cdeded1488d130d0c24a937ea357ef3
Javier <javierreyesperdomo@gmail.com>
2026-02-27 09:15:38 +0000

    limpiador.sh hecho

--- limpiador.sh
+++ limpiador.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/bash
 mkdir -p IMGS DOCS TXTS PDFS VACIOS
-IMGS=0
-DOCS=0
-TXTS=0
-PDFS=0
-VACIOS=0
-for archivo in "Descargas"/*; do
+ContIMGS=0
+ContDOCS=0
+ContTXTS=0
+ContPDFS=0
+ContVACIOS=0
+for archivo in "/home/oem/SSF"/*; do
   if [ -f "$archivo" ]; then
     case "$archivo" in
-      (*.jpg|*.png|*.gif)
-	mv "$archivo" IMGS/
-	((IMGS++))
-	;;
-      (*.docx|*.odt)
-	mv "$archivo" DOCS/
-	((DOCS++))
-	;;
-      (*.txt)
-	mv "$archivo" TXTS/
-	((TXTS++))
-	;;
-      (*.pdf)
-	mv "$archivo" PDFS/
-	((PDFS++))
-	;;
-	esac
-	fi
-	done
-
-find . -type f -empty -exec mv {} VACIOS/ \;
+      *.jpg|*.png|*.gif)
+        mv "$archivo" IMGS/
+        ((ContIMGS++))
+        ;;
+      *.docx|*.odt)
+        mv "$archivo" DOCS/
+        ((ContDOCS++))
+        ;;
+      *.txt)
+        mv "$archivo" TXTS/
+        ((ContTXTS++))
+        ;;
+      *.pdf)
+        mv "$archivo" PDFS/
+        ((ContPDFS++))
+        ;;
+    esac
+  fi
+done
+find "/home/oem/SSF"  -type f -size 0 -exec mv {} VACIOS/ \;
+((ContVACIOS++))
+echo "Archivos movidos:"
+echo "Imágenes: "$ContIMGS
+echo "Documentos: "$ContDOCS
+echo "Textos: "$ContPDFS
+echo "PDFS: "$ContPDFS
+echo "Archivos vacíos: "$ContVACIOS
